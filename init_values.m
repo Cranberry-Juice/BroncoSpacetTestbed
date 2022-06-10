3d5c0c088742ed14766403aed1d746aead759679
Cranberry-Juice <josepfamador2000@gmail.com>
2022-06-10 15:06:25 -0700

    quantized ABS step size

--- init_values.m
+++ init_values.m
@@ -8,14 +8,32 @@
 % TB: Test Bed
 % In.: Inertia
 % ABS: Autmatic Balance System
-m_sc = 6; % [kg] Mass of test article.
-I_sc = [130.34 3.01 10.52
-        3.02 174.64 -0.4
-        10.52 -0.4 181.23]; % In. of Spacecraft. Initial Guess
+
+%% Spacecraft Test Aritcle Mass Properties Taken from BS-1 CAD 6/10/22
+m_sc = 2.018492; % [kg] Mass of test article.
+
+% Units: g * mm^2
+Ixx = 6.105e6;
+Iyy = 5.865e6;
+Izz = 3.635e6;
+
+Ixy = -1.756e5;
+Ixz = -90573.811;
+
+Iyx = Ixy;
+Iyz = 59661.642;
+
+Izx = Ixz;
+Izy = Iyz;
+
+
+I_sc = [Ixx Ixy Ixz
+        Iyx Iyy Iyz
+        Izx Izy Izz] * ((1/1000) * (1/1000)^2); %kg * m^2
 
 I_tb = TBInertia; % In. of Testbed w/o balance masses. Initial guess.
 
-r_off = [-1 -2 -5] / 100; % cm conv. to meter. Initial CG offest from CoR of SC
+r_off = [-1; 5; -2;] / 100; % cm conv. to meter. Initial CG offest from CoR of SC
 R_o = m_sc * r_off; % eq 3 m_total - mb = m_sc
 %% Automatic Mass Balance System
 % Assuming testbed's CG is alligned with CoR without test article in place.
@@ -40,6 +58,8 @@ m = mb + m_sc; % [kg] Total of mass of ABS + SC
 d = [dx dy dz]; % Initial offset of balance masses. Place in vector for
 % convinience
 miU_mat_inv = inv([mx*ux' my*uy' mz*uz']); %inverse matrix of directional vectors U. See notes
+
+
 % Alternative Values with manufacturing errors
 % rhox_n= rhox + [0.001 0.002 0.003] % [cm]
 % rhoy_n= rhox + [0.001 0.002 0.003] % [cm] 
@@ -47,3 +67,5 @@ miU_mat_inv = inv([mx*ux' my*uy' mz*uz']); %inverse matrix of directional vector
 % ux_n= ux + [0.001 0.002 0.003] % [unitless]
 % uy_n= uy + [0.001 0.002 0.003] % [unitless]
 % uz_n= uz + [0.001 0.002 0.003] % [unitless] 
+
+stepsz = 1.5e-6; %m Smallest Step size possible for ABS steppers
